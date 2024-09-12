284321687f62f01ae20ce96854c89b31f418cecb
Brett <brett.whynot@gmail.com>
2024-09-12 11:33:04 +1000

    Fix build

--- .build.sh
+++ .build.sh
@@ -3,6 +3,6 @@ cd "${0%/*}"
 OUTPUT="${1:-TA-racing-telemetry.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=local --exclude=package.json --overwrite TA-racing-telemetry
